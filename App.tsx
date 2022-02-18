24c7068068ddab6abfebc668c8048f4c98d85b57
Peter Tellgren <peter.tellgren@griffeye.com>
2022-02-18 13:32:38 +0200

    Add challenge

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Container from '@mui/material/Container';
+import Solution from './Solution';
+
+const App = () => {
+  return (
+    <Container>
+      <Solution />
+    </Container>
+  );
+};
+
+export default App;
